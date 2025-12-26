5320c7e15f96e4cfa02ee40a9b88449bf025f4e9
Muhammad Sarim <sarimkhan13579@gmail.com>
2025-12-27 01:11:17 +0500

    Add initial cookie file and log entries for daemon warnings
    - Created a new cookie file at .turbo/cookies/1.cookie.
    - Added log entries for daemon warnings indicating that the daemon is already running for the dates 2025-12-25 and 2025-12-26.

--- apps/api/index.js
+++ apps/api/index.js
@@ -5,23 +5,35 @@ import userRoutes from './router/User.routes.js';
 import projectRoutes from './router/Project.routes.js';
 import aiRoutes from './router/ai.codeExplanation.routes.js';
 import bodyParser from 'body-parser';
+import cors from 'cors'
+
 const app = express();
+
+
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  
+}))
 
 app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(cookieParser());
 
+
+
+
 app.use("/", aiRoutes)
 app.use("/projects",projectRoutes)
 app.use("/",userRoutes)
 
 
+
+
 app.get("/", (req, res) => {
   res.send("API running successfully 🚀");
 });
-
-
-
 
 app.listen(5000, () => console.log("Server running on port 5000"));
--- apps/api/utility/redis.js
+++ apps/api/utility/redis.js
@@ -1,24 +1,24 @@
-import Redis from 'ioredis';
-import { Queue } from 'bullmq'
+  import Redis from 'ioredis';
+  import { Queue } from 'bullmq'
 
-const redis = new Redis({
-  host: 'redis-13299.c239.us-east-1-2.ec2.cloud.redislabs.com',
-  port:  13299,
+  const redis = new Redis({
+    host: 'redis-13299.c239.us-east-1-2.ec2.cloud.redislabs.com',
+    port:  13299,
 
-  password: 'Ly4YUbzyLSFcPfLl20vp2Tcgv2dtwLE0',
-skipConfigValidation: true
-});
+    password: 'Ly4YUbzyLSFcPfLl20vp2Tcgv2dtwLE0',
+  skipConfigValidation: true
+  });
 
-redis.on('connect', () => {
-  console.log('Connected to Redis!');
-});
+  redis.on('connect', () => {
+    console.log('Connected to Redis!');
+  });
 
-redis.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
+  redis.on('error', (err) => {
+    console.error('Redis connection error:', err);
+  });
 
-export const explanationQueue = new Queue('explanation-jobs', {
-  connection: redis
-});
+  export const explanationQueue = new Queue('explanation-jobs', {
+    connection: redis
+  });
 
-export default redis;
+  export default redis;
